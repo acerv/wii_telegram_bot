4252ed63a5209bb015a8b79925d0541fb5ffa66b
Andrea Cervesato <andrea.cervesato@mailbox.org>
2017-02-02 14:26:29 +0100

    Version 2.1

--- wiibot.py
+++ wiibot.py
@@ -2,6 +2,11 @@ import sys
 import logging
 import random
 import telebot
+import urlparse
+import urllib
+import os
+from imgurpython import ImgurClient
+from helpers import get_config, get_input
 
 # setup logger
 logger = logging.getLogger('wiibot')
@@ -10,9 +15,6 @@ myhandler = logging.StreamHandler()  # writes to stderr
 myformatter = logging.Formatter(fmt='%(levelname)s: %(message)s')
 myhandler.setFormatter(myformatter)
 logger.addHandler(myhandler)
-
-# put here the bot's token
-TOKEN='<token here>'
 
 # command description used in the "help" command
 commands = {      
@@ -27,11 +29,24 @@ commands = {
     'irc_quote':
         'Show a quote from the old IRC channel',
     'ftttt':
-        'FTTTT FTTTT'
+        'FTTTT FTTTT',
+    'russia':
+        'Why Russia is amazing',
+    'startrek':
+        'Star Trek gifs'
 }
 
+# read configuration file
+logger.debug('reading config.ini..')
+
+config = get_config()
+config.read('config.ini')
+
 # initialize bot
-bot = telebot.TeleBot(TOKEN)
+logger.debug('initialize bot..')
+
+token = config.get('telegram', 'token')
+bot = telebot.TeleBot(token)
 
 #############
 # UTILITIES #
@@ -45,11 +60,46 @@ def print_error(message, error):
 def show_sticker(name, cid):
     logger.debug('show_sticker: showing '+name+' sticker')
 
-    sticker_path = 'data/'+name+'.webp'
-    with open(sticker_path, 'rb') as stickers:
-        bot.send_sticker(cid, stickers)
+    try:
+        sticker_path = 'data/'+name+'.webp'
+        with open(sticker_path, 'rb') as stickers:
+            bot.send_sticker(cid, stickers)
 
-    logger.debug('show_sticker:'+name+' sticker shown')
+        logger.debug('show_sticker:'+name+' sticker shown')
+    except Exception, ex:
+        print_message(m, "%s" % ex)
+
+def show_subreddit_gallery(m, gallery):
+    logger.debug('show_subreddit_gallery: fetching image from imgur')
+
+    picfile = 'data/'+gallery
+
+    try:
+        logger.debug('show_subreddit_gallery: reading imgur credentials..')
+        client_id = config.get('imgur', 'client_id')
+        client_secret = config.get('imgur', 'client_secret')
+
+        logger.debug('show_subreddit_gallery: getting imgur images urls..')
+        client = ImgurClient(client_id, client_secret)
+        items = client.subreddit_gallery(
+            gallery, 
+            sort='time', 
+            window='week', 
+            page=0)
+        item_num = random.randint(1, len(items))
+        item = items[item_num]
+
+        if item.animated:
+            # !!! HACK !!! 
+            gifvtogif = item.gifv[:-1]
+            bot.send_message(m.chat.id, gifvtogif)
+        else:
+            bot.send_message(m.chat.id, item.link)
+
+        logger.debug('show_subreddit_gallery: image sent')
+    except Exception, ex:
+        print_message(m, "%s" % ex)
+        logger.exception(ex)
 
 ####################
 # COMMANDS SECTION #
@@ -58,15 +108,17 @@ def show_sticker(name, cid):
 def command_help(m):
     logger.debug('command_help: printing help message')
     
-    cid = m.chat.id
-    help_text = "The following commands are available: \n"
-    for key in commands:
-        help_text += "/" + key + ": "
-        help_text += commands[key] + "\n"
+    try:
+        help_text = "The following commands are available: \n"
+        for key in commands:
+            help_text += "/" + key + ": "
+            help_text += commands[key] + "\n"
 
-    bot.send_message(cid, help_text)
+        print_message(m, help_text)
 
-    logger.debug('command_help: help message printed')
+        logger.debug('command_help: help message printed')
+    except Exception, ex:
+        print_message(m, "%s" % ex)
 
 @bot.message_handler(commands=['lamerda'])
 def command_lamerda(m):
@@ -84,37 +136,47 @@ def command_bycicle(m):
 def command_irc_quote(m):
     logger.debug('command_irc_quote: reading the quotes file')
 
-    with open('data/quotes.txt', 'r') as fquotes:
-        num_lines = sum(1 for line in fquotes)
-        num_rands = random.randint(1, num_lines)
+    try:
+        with open('data/quotes.txt', 'r') as fquotes:
+            num_lines = sum(1 for line in fquotes)
+            num_rands = random.randint(1, num_lines)
 
-        logger.debug('command_irc_quote: ready to send quote #'+str(num_rands))
+            logger.debug('command_irc_quote: to send quote #'+str(num_rands))
 
-        text = ''
+            text = ''
 
-        fquotes.seek(0)
-        for i, line in enumerate(fquotes):
-            if i == num_rands - 1:
-                text = line
-                break
+            fquotes.seek(0)
+            for i, line in enumerate(fquotes):
+                if i == num_rands - 1:
+                    text = line
+                    break
 
-        if not text:
-            print_error(m, 'Quote not found')
-        else:
-            print_message(m, text)
+            if not text:
+                print_error(m, 'Quote not found')
+            else:
+                print_message(m, text)
 
-    logger.debug('command_irc_quote: quote sent')
+        logger.debug('command_irc_quote: quote sent')
+    except Exception, ex:
+        print_message(m, "%s" % ex)
 
 @bot.message_handler(commands=['ftttt'])
 def command_ftttt(m):
-    logger.debug('command_ftttt: sending picture')
+    bot.send_message(m.chat.id, '@valedix https://i.imgur.com/3STgUHv.jpg')
 
-    with open('data/ftttt.jpeg', 'rb') as fftttt:
-        bot.send_photo(m.chat.id, fftttt)
+@bot.message_handler(commands=['russia'])
+def command_russia(m):
+    show_subreddit_gallery(m, 'ANormalDayInRussia')
 
-    logger.debug('command_ftttt: picture sent')
+@bot.message_handler(commands=['startrek'])
+def command_startrek(m):
+    show_subreddit_gallery(m, 'startrekgifs')
 
 # start listening
-logger.debug('Start bot polling...')
+logger.debug('start bot polling..')
 
-bot.polling()
+try:
+    bot.polling()
+except Exception, ex:
+    print_message(m, "%s" % ex)
+    logger.exception(ex)
